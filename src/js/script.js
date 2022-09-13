7877c42b77e411eacb2e58f353bb608bfb70e910
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-09-13 21:29:51 +0800

    finishing all features

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,40 @@
-window.addEventListener('load', function () {
-	const hamburgerMenu = this.document.querySelector('.hamburger-menu');
+const removeNavBg = () => document.querySelector('.navbar').classList.remove('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
+const addNavBg = () => document.querySelector('.navbar').classList.add('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
+
+window.addEventListener('scroll', function () {
+	Math.floor(this.document.documentElement.scrollTop) > 80 ? addNavBg() : removeNavBg();
+
+	const hamburgerMenu = document.querySelector('.hamburger-menu');
 	hamburgerMenu.addEventListener('click', function () {
 		hamburgerMenu.classList.toggle('nav-menu');
 	});
+
+	const navItem = document.querySelectorAll('.nav-item');
+	navItem.forEach((item) => {
+		item.addEventListener('click', function () {
+			hamburgerMenu.classList.remove('nav-menu');
+		});
+	});
+
+	activeLink();
 });
 
-window.addEventListener('scroll', function () {
-	if (Math.floor(this.document.documentElement.scrollTop) > 152) {
-		this.document.querySelector('.navbar').classList.add('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
-	} else {
-		this.document.querySelector('.navbar').classList.remove('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
-	}
-});
+const navLinkContents = document.querySelectorAll('.nav-link-content');
+function activeLink() {
+	let current = '';
+
+	navLinkContents.forEach((content) => {
+		const sectionTop = content.offsetTop;
+		if (scrollY >= sectionTop - 250) {
+			current = content.getAttribute('id');
+		}
+	});
+
+	const navLinkItems = document.querySelectorAll('.nav-item a');
+	navLinkItems.forEach((item) => {
+		item.classList.remove('active');
+		if (item.classList.contains(current)) {
+			item.classList.add('active');
+		}
+	});
+}
