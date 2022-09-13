577f20f40dd6c3a258b4bdc3b9ae3e4dd66f5125
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-09-13 21:49:58 +0800

    modifying javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,22 @@
+window.addEventListener('scroll', function () {
+	Math.floor(this.document.documentElement.scrollTop) > 80 ? addNavBg() : removeNavBg();
+	activeLink();
+});
+
 const removeNavBg = () => document.querySelector('.navbar').classList.remove('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
 const addNavBg = () => document.querySelector('.navbar').classList.add('bg-[#fbebf6e6]', 'backdrop-blur-lg', 'shadow-sm');
 
-window.addEventListener('scroll', function () {
-	Math.floor(this.document.documentElement.scrollTop) > 80 ? addNavBg() : removeNavBg();
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+console.log(hamburgerMenu);
+hamburgerMenu.addEventListener('click', function () {
+	hamburgerMenu.classList.toggle('nav-menu');
+});
 
-	const hamburgerMenu = document.querySelector('.hamburger-menu');
-	hamburgerMenu.addEventListener('click', function () {
-		hamburgerMenu.classList.toggle('nav-menu');
+const navItem = document.querySelectorAll('.nav-item');
+navItem.forEach((item) => {
+	item.addEventListener('click', function () {
+		hamburgerMenu.classList.remove('nav-menu');
 	});
-
-	const navItem = document.querySelectorAll('.nav-item');
-	navItem.forEach((item) => {
-		item.addEventListener('click', function () {
-			hamburgerMenu.classList.remove('nav-menu');
-		});
-	});
-
-	activeLink();
 });
 
 const navLinkContents = document.querySelectorAll('.nav-link-content');
